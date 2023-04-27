a55331bfc12b6696616f79d41639c5f6b7bd0a3e
Esla <suarezesla@gmail.com>
2023-04-27 21:31:58 +0200

    creacion de formulario usuario y conexion a bd

---
+++ assets/adopciones.sql
@@ -0,0 +1,43 @@
+
+CREATE table refugio
+(
+nif varchar(9) PRIMARY KEY,
+CHECK(LENGTH(nif)=9),
+nom_refugio varchar(200) NOT NULL,
+direccion varchar(200) NOT NULL,
+telefono varchar(9) NOT NULL,       
+CHECK(LENGTH(telefono)=9),
+email varchar(100) NOT NULL
+);
+
+DROP TABLE IF EXISTS animal;
+CREATE table animal
+(
+    microchip varchar(15) PRIMARY KEY,
+    CHECK(LENGTH(microchip)>=10),
+    nif_refugio varchar(200) NOT NULL,
+    especie enum("perro", "gato", "conejo", "ave", "equino") NOT NULL,
+    raza varchar(100) NOT NULL,
+    nombre varchar(50) NOT NULL,
+    sexo enum("macho", "hembra") NOT NULL,
+    fecha_nac date NOT NULL,
+    tamano varchar(50),
+    peso decimal(2,2),
+    CONSTRAINT fk_animal_refugio
+    FOREIGN KEY (nif_refugio)
+    REFERENCES refugio(nif)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+    );
+
+    
+DROP TABLE IF EXISTS usuario;
+CREATE table usuario
+(
+    email varchar(30) PRIMARY KEY,
+    nombre varchar(50) NOT NULL,
+    apellidos varchar(100) NOT NULL,
+    nick varchar(15) NOT NULL,
+    contrasena varchar(15) NOT NULL,
+    intereses varchar(50)
+);
--- src/registro.php
+++ src/registro.php
@@ -1,4 +1,40 @@
 <?php
 include 'index.html';
-echo '<h1>Estamos en registro.php</h1>'
 ?>
+
+<div class='formulario_usuarios'>
+  <form action="procesar_form_usuario.php" method="POST">
+  <h3>Registro de usuarios</h3>
+  <div class="form-group">
+    <label for="email">Email address</label>
+    <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="nombre">Nombre</label>
+    <input type="nombre" name="nombre" class="form-control" id="nombre"  placeholder="Introduce tu nombre">
+  </div>
+  <div class="form-group">
+    <label for="apellidos">Apellidos</label>
+    <input type="apellidos" name="apellidos" class="form-control" id="apellidos"  placeholder="Introduce tus apellidos">
+  </div>
+  <div class="form-group">
+    <label for="nick">Nick</label>
+    <input type="nick" name="nick" class="form-control" id="nick"  placeholder="Introduce tu nick">
+  </div>
+  <!-- <div class="form-group">
+    <label for="contrasena">Password</label>
+    <input type="contrasena" class="form-control" id="contrasena" placeholder="Password">
+    <small id="contrasena_help" name="contrasena"  class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div> -->
+  <div class="form-group">
+  <label for="contrasena">Contraseña:</label>
+    <input type="password" name="contrasena" class="form-control" id="contrasena" required><br>
+</div>
+  <div class="form-group">
+    <label for="intereses">Intereses</label>
+    <input type="intereses" name="intereses" class="form-control" id="intereses"  placeholder="Introduce tu nick">
+  </div>
+  <button type="submit" value="Registrarse">Submit</button>
+</form>
+</div>
