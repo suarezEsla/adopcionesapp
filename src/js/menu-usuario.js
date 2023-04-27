5f2d8fe46de3dd2abddcab6c84f36425a5b59257
Esla <suarezesla@gmail.com>
2023-04-27 16:43:42 +0200

    menu usuario funcional

---
+++ src/js/menu-usuario.js
@@ -0,0 +1,19 @@
+const userImg = document.getElementById("user-img");
+const userMenu = document.getElementById("user-menu");
+
+userImg.addEventListener("click", function() {
+  userMenu.classList.toggle("show");
+});
+
+document.addEventListener("click", function(event) {
+  if (!event.target.closest(".user-container")) {
+    userMenu.classList.remove("show");
+  }
+});
+
+
+
+
+
+
+
