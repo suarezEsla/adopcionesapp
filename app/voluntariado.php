37d42a16ebbe68ded084f34e54cd37a2f0be95df
Esla <suarezesla@gmail.com>
2023-05-09 19:14:20 +0200

    voluntariado

---
+++ app/voluntariado.php
@@ -0,0 +1,31 @@
+<?php
+ include 'index.html';
+ ?>
+ <section class="voluntariado py-5">
+  <div class="container">
+    <h2 class="text-center mb-5">¡Haz la diferencia! Sé voluntario</h2>
+    <div class="row">
+      <div class="col-md-6">
+        <h3>Requisitos para ser voluntario</h3>
+        <ul class="list-unstyled">
+          <li>Debes tener al menos 18 años de edad, o tener la autorización por escrito de un padre o tutor legal.</li>
+          <li>Debes tener una actitud positiva y amor por los animales.</li>
+          <li>Debes poder trabajar en equipo y seguir las instrucciones dadas por el personal del refugio.</li>
+          <li>Debes completar el formulario de solicitud de voluntario y asistir a una sesión de orientación para voluntarios.</li>
+        </ul>
+      </div>
+      <div class="col-md-6">
+        <h3>Tipos de trabajo voluntario</h3>
+        <ul class="list-unstyled">
+          <li>Paseadores de perros</li>
+          <li>Cuidadores de gatos</li>
+          <li>Trabajadores de eventos</li>
+          <li>Ayuda administrativa</li>
+          <li>Ayuda en mantenimiento</li>
+        </ul>
+        <p>Si estás interesado en ser voluntario en nuestro refugio, por favor completa nuestro formulario de solicitud de voluntariado. Después de enviar tu solicitud, nos pondremos en contacto contigo para programar una sesión de orientación para voluntarios. ¡Gracias por tu interés en ayudar a nuestros animales!</p>
+        <a href="#" class="btn btn-primary mt-3">Solicitud de voluntariado</a>
+      </div>
+    </div>
+  </div>
+</section>
