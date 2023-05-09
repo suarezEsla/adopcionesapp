672acef6a610935695973fc4fbf3ee17de73bce6
Esla <suarezesla@gmail.com>
2023-05-09 08:52:28 +0200

    funcion en index para ocultar carrusel, no funciona

---
+++ app/js/ocultar_index.js
@@ -0,0 +1,4 @@
+if (window.location.pathname === "index.html") {
+    document.getElementsByClassName("contenido-index").style.display = "block";
+} else {
+    document.getElementsByClassName("contenido-index").style.display = "none";}
