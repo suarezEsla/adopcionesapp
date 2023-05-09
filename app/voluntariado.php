9e26d669cf8f82e8f7198b8d0aa30a0d51f09bfd
Esla <suarezesla@gmail.com>
2023-05-09 19:54:57 +0200

    voluntariado

--- app/voluntariado.php
+++ app/voluntariado.php
@@ -1,9 +1,10 @@
+
 <?php
  include 'index.html';
  ?>
  <div id="contenido">
  <div class="voluntariado">
-<div class="row" >
+<div class="row">
   <div class="col-md-6">
     <h3>Requisitos para ser voluntario</h3>
     <ul class="list-unstyled">
